75c521320d7b06ede4243c0d99cd36e26e924ee9
metheis <metheis@gmail.com>
2020-08-16 23:38:48 -0700

    add subscriberd to broker

--- broker/cloud/python/brokerd.py
+++ broker/cloud/python/brokerd.py
@@ -1,6 +1,7 @@
 import logging
 
 from broker.protos import brokerd_pb2, brokerd_pb2_grpc
+from broker.protos.subscriberdb_pb2_grpc import SubscriberDBStub
 
 
 class BrokerdRpcServicer(brokerd_pb2_grpc.BrokerdServicer):
@@ -8,8 +9,10 @@ class BrokerdRpcServicer(brokerd_pb2_grpc.BrokerdServicer):
     Brokerd rpc server.
     """
 
-    def __init__(self):
+    def __init__(self, subscriberdb_stub: SubscriberDBStub):
         logging.info("starting brokerd servicer")
+        self._subscriberdb_stub = subscriberdb_stub
+
 
     def add_to_server(self, server):
         brokerd_pb2_grpc.add_BrokerdServicer_to_server(self, server)
@@ -21,4 +24,23 @@ class BrokerdRpcServicer(brokerd_pb2_grpc.BrokerdServicer):
         #  - request AuthenticationInformation to subscriberdb;
         #  - sign the response
         #  - create BrokerAuthenticationInformationAnswer and return
-        pass
+        subd_answer = self._subscriberdb_stub.AuthenticationInformation(request)
+        aia = s6a_proxy_pb2.AuthenticationInformationAnswer()
+        aia.error_code = subd_answer.error_code
+        aia.eutran_vectors = subd_answer.eutran_vectors
+        logging.info("Auth Msg Received")
+        return aia
+
+    except CryptoError as e:
+        logging.error("Auth error for %s: %s", imsi, e)
+        metrics.S6A_AUTH_FAILURE_TOTAL.labels(
+            code=metrics.DIAMETER_AUTHENTICATION_REJECTED).inc()
+        aia.error_code = metrics.DIAMETER_AUTHENTICATION_REJECTED
+        return aia
+
+    except SubscriberNotFoundError as e:
+        logging.warning("Subscriber not found: %s", e)
+        metrics.S6A_AUTH_FAILURE_TOTAL.labels(
+            code=metrics.DIAMETER_ERROR_USER_UNKNOWN).inc()
+        aia.error_code = metrics.DIAMETER_ERROR_USER_UNKNOWN
+        return aia
