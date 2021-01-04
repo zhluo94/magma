7b0d10bd7da0e7a7b3976a2b862202fe326fc920
zhluo94 <zhluo@berkeley.edu>
2021-01-03 19:40:31 -0800

    nits

--- benchmarks/ho_proxy/collect_container_logs.sh
+++ benchmarks/ho_proxy/collect_container_logs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 for app_name in iperf ping; do
-  for fn in $(docker exec -it uec ls -d ${app_name}_log_${TEST_SETUP}_*); do
+  for fn in $(docker exec -it uec sh -c "ls -d ${app_name}_log_${TEST_SETUP}_*"); do
     fn=$(echo $fn | tr -d '\r')
     if [ -f "$fn" ]; then
       echo "${fn} exists"
     else
+      echo "Copy ${fn} from uec"
       docker cp uec:/${fn} .
     fi
   done
