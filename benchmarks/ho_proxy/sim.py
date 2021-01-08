d37fc007d58d407b1a46dc0bd9f8dc250c4bfc47
metheis <metheis@gmail.com>
2021-01-08 08:13:49 +0000

    finish draft sip sim call in sim.py

--- benchmarks/ho_proxy/sim.py
+++ benchmarks/ho_proxy/sim.py
@@ -89,6 +89,37 @@ def run_sip():
                 _ip_pool = iter(range(5, 128))
                 ip = _ip_base + str(next(_ip_pool))
 
+            app.start_sip()
+
+            # Handover at 5 seconds
+            time.sleep(5)
+            t = datetime.now().time()
+            print("IP Handover: " + ip + ", Time: " + str(t))
+            app.do(new_ip=ip, lat=i)
+            # Handover at 10 seconds
+            try:
+                ip = _ip_base + str(next(_ip_pool))
+            except StopIteration:
+                _ip_pool = iter(range(5, 128))
+                ip = _ip_base + str(next(_ip_pool))
+            time.sleep(5)
+            t = datetime.now().time()
+            print("IP Handover: " + ip + ", Time: " + str(t))
+            app.do(new_ip=ip, lat=i)
+            # Handover at 15 seconds
+            try:
+                ip = _ip_base + str(next(_ip_pool))
+            except StopIteration:
+                _ip_pool = iter(range(5, 128))
+                ip = _ip_base + str(next(_ip_pool))
+            time.sleep(5)
+            t = datetime.now().time()
+            print("IP Handover: " + ip + ", Time: " + str(t))
+            app.do(new_ip=ip, lat=i)
+            # Wait for 5 more seconds for iperf stream to finish
+            time.sleep(6)
+
+
 
 
 if __name__ == "__main__":
