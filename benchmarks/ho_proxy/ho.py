03656a94401a0773a68eff44ff5d569bd0c9603e
silveryfu <root@ip-172-31-4-65.us-west-1.compute.internal>
2021-01-14 05:07:52 +0000

    [bm] fix container ip change

--- benchmarks/ho_proxy/ho.py
+++ benchmarks/ho_proxy/ho.py
@@ -11,17 +11,15 @@ def change_con_ip(new_ip, name="uec", ifac="eth0",
                   netmask="255.255.0.0", gw="172.17.0.1",
                   lat=0, msg=""):
     _del = "ifconfig {} 0.0.0.0 netmask 0.0.0.0".format(ifac)
+    _sleep = "sleep {}".format(lat)
     _add = "ifconfig {} {} netmask {} broadcast 0.0.0.0".format(ifac, new_ip, netmask)
     _gw = "route add default gw {}".format(gw)
-    _exec = "sudo docker exec -it {} {} > /dev/null 2>&1"
+    _msg = "" if msg == "" else "echo {}".format(msg)
+    _exec = "docker exec -it {} /bin/bash -c '{}' > /dev/null 2>&1"
 
-    subprocess.Popen(";".join([
-        _exec.format(name, _del),
-        "sleep {}".format(lat),
-        _exec.format(name, _add),
-        _exec.format(name, _gw),
-        "" if msg == "" else "echo {}".format(msg),
-    ]), shell=True)
+    _cmd = _exec.format(name, ";".join([_del, _sleep, _add, _gw, _msg]))
+    print(_cmd)
+    subprocess.Popen(_cmd, shell=True)
 
 
 def do(*args, f=change_con_ip, **kwargs):
@@ -29,4 +27,4 @@ def do(*args, f=change_con_ip, **kwargs):
 
 
 if __name__ == "__main__":
-    do("172.17.0.128", lat=1)
+    do("172.17.0.128", f=change_con_ip, lat=10)
